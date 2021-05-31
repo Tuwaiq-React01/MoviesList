8d843f430487417341b8dbbccef485eb675e3deb
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-05-31 22:45:48 +0300

    movie

---
+++ movie/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieList from './movieList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+ 
+  return (
+    <div  className="B">
+      <MovieList/>
+    </div>
+  );
+}
+export default App;
