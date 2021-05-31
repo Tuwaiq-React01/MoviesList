d3b90b5ba158a2d83bb2202e5b94b44d60ca1972
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-05-31 12:07:06 +0300

    fix design

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,16 +5,18 @@ import movieList from './seeds'
 function App() {
 
   const movieListView = movieList.map((movie,i) => {
-    return <div class="col-sm-4">
+    return <div class="col">
     <MovieCard key={i} movie={movie}/>
     </div>
   });
 
   return (
-    <div class="container">
+    <div id="body" class="d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-center align-items-center">
       <div class="row">
         {movieListView}
       </div>
+    </div>
     </div>
   );
 }
--- movie-app/src/MovieCard.jsx
+++ movie-app/src/MovieCard.jsx
@@ -7,17 +7,17 @@ class MovieCard extends Component {
     render() {
         const {poster, name, year, info}= this.props.movie;
         return (
-            <div class="card" style={{ width: '30rem' }}>
+            <div class="card mb-4 bg-light" style={{ width: '35rem' }}>
                <div class="row">
                <div class="col">
                <img class="card-img-top" src={poster} alt="Card image cap" />
                </div>
                <div class="col">
-               <div class="card-body">
+               <div class="card-body bg-light">
                     <h5 class="card-title">{name}</h5>
                     <h6>{year}</h6>
                     <p class="card-text">{info}</p>
-                    <a href="#" class="btn btn-primary">Book</a>
+                    <a href="#" class="btn btn-warning">Book</a>
                 </div>
                </div>
                </div>
