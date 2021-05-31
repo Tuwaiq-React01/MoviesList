d48e6a513bb5fe67380994641ac9d480f7ce519e
shahad <shahad@test>
2021-05-31 21:29:42 +0300

    first commit

---
+++ reactProject1/hello/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Movie from "./Movie";
+import { Component } from "react";
+
+class App extends Component {
+  render(){
+
+  return (
+    <div className="App">
+      <h1>Movie</h1>
+      <Movie />
+   
+    </div>
+  );
+}}
+
+export default App;
---
+++ reactProject1/hello/src/FirstCom.js
@@ -0,0 +1,7 @@
+//import {Component} from "react";
+class FirstCom extends React.Component{
+render(){
+
+}
+
+}
