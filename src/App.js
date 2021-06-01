0e21c9a83d0d405be8d65c3991ff560eac88f664
Sara Abdulmajeed <Sara.Alnasser@tuwaiq.edu.sa>
2021-06-01 07:52:21 +0300

    MovieList updateed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie.js'
 import './Movie.css'
-function App() {
+import { Component } from 'react';
+import Title from './Title';
+
+
+export default class App extends Component {
+  render(){
   return (
     <div className="App">
-      
-      <Movie/>
+      <Title title="Welcome To My Movie List" style="  align-content: center;
+"/>
+      <Movie />
     </div>
   );
 }
-
-export default App;
+}
